a5bd8f65b7c50fb3767c53e52b47467513ec43a1
chaudhary shivam akhilesh <shivamkota1999@gmail.com>
2020-07-10 23:13:43 +0530

    4

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ class App extends React.Component {
         <Cards data={data} />
         <CountryPicker handleCountryChange={this.handleCountryChange} />
         <Chart data={data} country={country} />
+        <header>DEvelOped By ShiVAm</header>
       </div>
     );
   }
